e946ee85747a8d4ada7b8c7ffe4a4f621aaec37f
irfan.ms <irfan.ms@allbestsistem.com>
2025-07-20 22:15:58 +0700

    dynamic title right

--- src/app/app.breadcrumb.component.ts
+++ src/app/app.breadcrumb.component.ts
@@ -3,18 +3,29 @@ import { BreadcrumbService } from './breadcrumb.service';
 import { Subscription } from 'rxjs';
 import { MenuItem } from 'primeng/api';
 import { CanActiveGuardService } from './core/services/can-active-guard.service';
+import {NavigationEnd, Router} from '@angular/router';
+
 
 @Component({
     selector: 'app-breadcrumb',
     templateUrl: './app.breadcrumb.component.html'
 })
 export class AppBreadcrumbComponent implements OnDestroy {
-
+    titleRight: string = '';
     subscription: Subscription;
-
     items: MenuItem[];
     title: string;
-    constructor(public breadcrumbService: BreadcrumbService, private canActive: CanActiveGuardService) {
+    constructor(
+      public breadcrumbService: BreadcrumbService,
+      private canActive: CanActiveGuardService,
+      private router: Router) {
+        this.router.events.subscribe((resp: NavigationEnd) => {
+            if(resp.url && resp.url.includes('smi')) {
+                this.titleRight = 'Sustainability Maturity Index';
+            } else {
+                this.titleRight = 'Risk Maturity Index';
+            }
+        });
         this.canActive.titleComponent$.subscribe(resp => {
             this.title = resp;
         });
